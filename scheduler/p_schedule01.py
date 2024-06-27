f7b1e92d2b748880f0a5a94efbd4cfb5c8e77db0
bishimaru <a414510nee@yahoo.co.jp>
2024-06-27 22:01:06 +0900

    auto commit

--- scheduler/p_schedule01.py
+++ scheduler/p_schedule01.py
@@ -4,7 +4,7 @@ import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from widget import pcmax, happymail, func
-import h_footprint01
+import h_footprint1
 import sb_h_day_shift
 import s_check_mail_hpj
 from fst_mail_pcmax import template_multiple_fst_mail, chara_order_fstmail
