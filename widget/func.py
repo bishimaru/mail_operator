e2ef77339748dcbb1bf040a1afb91e2de88c17be
bishimaru <a414510nee@yahoo.co.jp>
2024-07-17 16:30:49 +0900

    auto commit

--- widget/func.py
+++ widget/func.py
@@ -127,25 +127,6 @@ def send_error(chara, error_message):
   smtpobj.close()
 
 def send_mail(message):
-  mailaddress = 'kenta.bishi777@gmail.com'
-  password = 'rjdzkswuhgfvslvd'
-  text = message
-  subject = "ハッピーメールサイト回し件数"
-  address_from = 'kenta.bishi777@gmail.com'
-  # address_to = "ryapya694@ruru.be"
-  address_to = "gifopeho@kmail.li"
-  smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
-  smtpobj.starttls()
-  smtpobj.login(mailaddress, password)
-  msg = MIMEText(text)
-  msg['Subject'] = subject
-  msg['From'] = address_from
-  msg['To'] = address_to
-  msg['Date'] = formatdate()
-  smtpobj.send_message(msg)
-  smtpobj.close()
-  time.sleep(2)
-  # <<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>
   mailaddress = setting.address_from
   password = setting.gmail_pass
   text = message
