eb6d6c1f5cf17d68f7f3ebf3faf26ea80e4bceed
bishimaru <a414510nee@yahoo.co.jp>
2024-07-25 17:55:06 +0900

    auto commit

--- s_check_mail_hpj.py
+++ s_check_mail_hpj.py
@@ -105,18 +105,18 @@ def check_mail():
         new_mail_lists = []
         debug = False
         #  # ハッピーメール
-        # try:
-        #     driver, wait = get_driver(debug)
-        #     happymail_new = happymail.check_new_mail(driver, wait, order_info[0])
-        #     if happymail_new:
-        #         new_mail_lists.append(happymail_new)
-        #     driver.quit()
-        # except Exception as e:
-        #     print(f"<<<<<<<<<<メールチェックエラー：ハッピーメール{order_info[0]}>>>>>>>>>>>")
-        #     print(traceback.format_exc())
-        #     func.send_error(f"メールチェックエラー：ハッピーメール{order_info[0]}", traceback.format_exc())
+        try:
+            driver, wait = get_driver(debug)
+            happymail_new = happymail.check_new_mail(driver, wait, order_info[0])
+            if happymail_new:
+                new_mail_lists.append(happymail_new)
+            driver.quit()
+        except Exception as e:
+            print(f"<<<<<<<<<<メールチェックエラー：ハッピーメール{order_info[0]}>>>>>>>>>>>")
+            print(traceback.format_exc())
+            func.send_error(f"メールチェックエラー：ハッピーメール{order_info[0]}", traceback.format_exc())
 
-        #     driver.quit()
+            driver.quit()
         # pcmax
         try:
             driver, wait = get_driver(debug)
